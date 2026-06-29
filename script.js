46518f93a49c4aacc731a20953f047bcdbacffd6
apasosby <henrrymoysesg@gmail.com>
2026-06-29 08:05:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const particles = document.getElementById("particles");
+
+    for (let i = 0; i < 80; i++) {
+
+        const dot = document.createElement("div");
+
+        dot.className = "particle";
+
+        dot.style.left = Math.random() * 100 + "%";
+        dot.style.top = Math.random() * 100 + "%";
+
+        dot.style.animationDelay = (Math.random() * 8) + "s";
+
+        particles.appendChild(dot);
+
+    }
+
+});
